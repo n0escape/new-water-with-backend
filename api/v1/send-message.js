6af89bca390fa3c6fa533ca3c547f3c58c4665e7
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-05-09 14:24:01 +0300

    fix cors issue with vercel

--- api/v1/send-message.js
+++ api/v1/send-message.js
@@ -8,17 +8,23 @@ const API_REQUEST_INTERVAL = 30 * 60 * 1000;
 let lastApiRequestTime = 0;
 
 module.exports = async (req, res) => {
-  const currentTime = Date.now();
-  if (currentTime - lastApiRequestTime < API_REQUEST_INTERVAL) {
-    return res.status(429).json({ error: 'Забагато запитів на відправку.' });
-  }
 
-  try {
-    const response = await axios.post(FORMSPREE_API_URL, req.body);
-    lastApiRequestTime = currentTime;
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Помилка відправки запиту.' });
-  }
+    // Устанавливаем заголовки CORS
+    res.setHeader('Access-Control-Allow-Origin', 'https://new-water.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    const currentTime = Date.now();
+    if (currentTime - lastApiRequestTime < API_REQUEST_INTERVAL) {
+        return res.status(429).json({ error: 'Забагато запитів на відправку.' });
+    }
+
+    try {
+        const response = await axios.post(FORMSPREE_API_URL, req.body);
+        lastApiRequestTime = currentTime;
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Помилка відправки запиту.' });
+    }
 };
