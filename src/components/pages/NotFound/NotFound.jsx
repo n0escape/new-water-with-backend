9342a6fe460dfe3135795ee5ae8b0184a6d67961
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-08-28 14:40:20 +0300

    stylized 404

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -1,19 +1,20 @@
 import { Link } from 'react-router-dom';
 import s from './NotFound.module.css'
+import fonts from '../../../generalStyles/Fonts.module.css'
 
 const NotFound = () => {
     return (
         <div className={s.notFound}>
             <div className={s.message}>
-                <h1>404</h1>
-                <p>
+                <h1 className={fonts.notFoundDisplay}>404</h1>
+                <p className={fonts.textMRegular}>
                     Нажаль ми не змогли знайти запитувану сторінку. 
                     Будь-ласка перевірте правильність посилання. 
                     За бажанням ви можете повернутися на головну сторінку сайту
                 </p>
             </div>
             <div className={s.btnMainContainer}>
-                <Link to="/#anchorAboutUs">Повернутися на головну</Link>
+                <Link className={fonts.lableS}to="/#anchorAboutUs">Повернутися на головну</Link>
             </div>
         </div>
     )
