f9958988fdad4092fc1ff9b1551d3509eaa893a5
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-08-28 15:49:56 +0300

    stylized about us

--- src/components/pages/MainPage/AboutUsSection/AboutUs.jsx
+++ src/components/pages/MainPage/AboutUsSection/AboutUs.jsx
@@ -1,46 +1,49 @@
 import React from 'react'
 import s from './AboutUs.module.css'
+import fonts from '../../../../generalStyles/Fonts.module.css'
 import { Link } from "react-router-dom"
 
 const AboutUs = ({aboutUs}) => (
     <section id={s.aboutUs}>
         <div className={s.details}>
           <div className={s.headline}>
-            <h1>{aboutUs.companyName}</h1>
+            <h1 className={fonts.display}>{aboutUs.companyName}</h1>
               <div className={s.slogan}>
                 {aboutUs.slogan.map((element,index) => (
-                  <p key={index}>{element}</p>
+                  <p className={index === 0 ? fonts.headerM : fonts.headerS}
+                    key={index}
+                  >
+                    {element}
+                  </p>
                 ))}
               </div>
           </div>
-          <p>
+          <p className={fonts.textMRegular}>
             {aboutUs.companyDescription.map((element, index) => (
               index === 0 || index % 2 === 0 
               ? <React.Fragment key={index}>{element}</React.Fragment>
-              :<strong key={index}>{element}</strong>
+              :<strong className={fonts.textMBold} key={index}>{element}</strong>
             ))}
           </p>
         </div>
         <div className={s.stepsBlock}>
           <div className={s.stepsSection}>
-            <div>
-              <h2>4 простих кроки до Вашої свердловини</h2>
-            </div>
+            <h2 className={fonts.headerS}>4 простих кроки до Вашої свердловини</h2>
             <div className={s.stepsContainer}>
               {aboutUs.mainSteps.map( step => (
                 <div key={step.title} className={s.stepItem}>
                   <div className={s.header}>
-                    <h3>{step.title}</h3>
+                    <h3 className={fonts.titleS}>{step.title}</h3>
                   </div>
                   <div className={s.info}>
-                    <p>{step.description}</p>
+                    <p className={fonts.textS}>{step.description}</p>
                   </div>
                 </div>
               ))}
             </div>
           </div>
           <div className={s.contactUsBtn}>
-            <Link to="/#anchorContactUs">Залишити замовлення</Link>
+            <Link className={fonts.lableLSemiBold} to="/#anchorContactUs">Залишити замовлення</Link>
           </div>
         </div>
       </section>
