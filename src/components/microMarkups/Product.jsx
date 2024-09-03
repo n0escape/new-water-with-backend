9d29521ff3f9e20ed4e1ab99ea043f18c8e46c20
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-09-03 15:46:46 +0300

    fixed product micromarkups

--- src/components/microMarkups/Product.jsx
+++ src/components/microMarkups/Product.jsx
@@ -1,16 +1,23 @@
 import { baseUrl } from "../../App";
 
+function extractPrice(priceString) {
+  // Используем регулярное выражение для поиска чисел в строке
+  const match = priceString.match(/\d+(\s?\d{3})*/);
+  // Если найдено совпадение, удаляем пробелы и возвращаем результат
+  return match ? match[0].replace(/\s/g, '') : null;
+}
+
 const Product = ({serviceData, canonicalUrl}) => {
     const product = {
         "@context": "http://schema.org",
         "@type": "Product",
         "name": `${serviceData.title}`,
-        "description": `${serviceData.description}`,
-        "image": `${baseUrl}${serviceData.photos[0]}`,
+        "description": `${serviceData.description.join('').trim()}`,
+        "image": `${baseUrl}${serviceData.photos[0].imageSrc}`,
         "offers": {
           "@type": "AggregateOffer",
           "availability": "http://schema.org/InStock",
-          "lowPrice": `${serviceData.minPrice}`, 
+          "lowPrice": `${extractPrice(serviceData.minPrice)}`, 
           "priceCurrency": "UAH",
           "url": `${canonicalUrl}`
         }
