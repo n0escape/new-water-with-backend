2beb4d6b649cf5d01e233ff1c40ea061306650fa
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-09-17 18:14:15 +0300

    contactus headers fix

--- src/components/pages/MainPage/ContactUsSection/ContactUs.jsx
+++ src/components/pages/MainPage/ContactUsSection/ContactUs.jsx
@@ -5,7 +5,7 @@ import fonts from '../../../../generalStyles/Fonts.module.css'
 const ContactUs = ({servicesList}) => (
     <section id={s.contactUs}>
         <div className={s.header}>
-            <h1 className={fonts.headerL}>Маєте запитання чи бажаєте замовити послугу?</h1>
+            <h2 className={`${s.headerH1} ${fonts.headerL}`}>Маєте запитання чи бажаєте замовити послугу?</h2>
             <h2 className={fonts.headerM}>Тоді просто залиште свою заявку через форму...</h2>
         </div>
         <ContactForm servicesList={servicesList}/>
